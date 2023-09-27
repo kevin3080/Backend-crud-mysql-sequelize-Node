f22020273f8663b37312a482e1a62d9f7494db58
Kevin Pernia <kevinpernia29@gmail.com>
2023-09-26 19:36:29 -0500

    contraseña agregada

--- database/db.js
+++ database/db.js
@@ -2,10 +2,15 @@ import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, '', {
-   host: process.env.DB_HOST,
-   port: process.env.DB_PORT,
-   dialect: 'mysql',
-});
+const db = new Sequelize(
+   process.env.DB_NAME,
+   process.env.DB_USER,
+   process.DB_PASSWORD,
+   {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      dialect: 'mysql',
+   }
+);
 
 export default db;
