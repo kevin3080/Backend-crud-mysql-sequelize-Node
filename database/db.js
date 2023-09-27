d08c91eca1d86807c385a5257894331d6ae5c846
Kevin Pernia <kevinpernia29@gmail.com>
2023-09-26 19:53:13 -0500

    contraseña corregida

--- database/db.js
+++ database/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 const db = new Sequelize(
    process.env.DB_NAME,
    process.env.DB_USER,
-   process.DB_PASSWORD,
+   process.env.DB_PASSWORD,
    {
       host: process.env.DB_HOST,
       port: process.env.DB_PORT,
